da11c88ae18a47ebf5c3058a6afc954a42c8ecbf
oneEyedBunny <allyson.short@gmail.com>
2018-07-16 19:03:50 -0700

    Create structure of index page. Add other base structure files

--- server.js
+++ server.js
@@ -3,11 +3,21 @@
 const express = require('express');
 const app = express();
 app.use(express.static('public'));
+const appRouter = require("./appRouter");
+
 
 if (require.main === module) {
   app.listen(process.env.PORT || 8080, function() {
     console.info(`Your app is listening on ${this.address().port}`);
   });
 }
+
+// app.get("/", (req, res) => {
+//   res.sendFile(__dirname + "/public/index.html");
+// });
+//
+// app.use("/posts", appRouter);
+
+
 
 module.exports = app;
--- test/test-server.js
+++ test/test-server.js
@@ -12,11 +12,31 @@ const should = chai.should();
 //lets us make http requests in our tests
 chai.use(chaiHttp);
 
-//normal test case for GET route
+//setup of endpoints
 describe('index page', function() {
   it('should return index.html', function() {
     return chai.request(app)
     .get('/')
+    .then(function(res) {
+      expect(res).to.have.status(200);
+    });
+  });
+});
+
+describe('posts page', function() {
+  it('should return post.html', function() {
+    return chai.request(app)
+    .get('/post.html')
+    .then(function(res) {
+      expect(res).to.have.status(200);
+    });
+  });
+});
+
+describe('profile page', function() {
+  it('should return results.html', function() {
+    return chai.request(app)
+    .get('/results.html')
     .then(function(res) {
       expect(res).to.have.status(200);
     });
